9cca2089b82c486a1e1a8e960d675ff2e2eb7918
Precious Damisa <preciousdamisa@gmail.com>
2021-11-23 11:37:59 +0100

    Change to using app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Hello there!' });
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log('Listening on', port);
+});
